5584cf90a28f4f94f04e2127f6eadb48dc39cb79
wnwngud <blane777@naver.com>
2025-10-19 18:21:30 +0900

    feat(cli): add user input/output

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      let inputStr = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+
+      MissionUtils.Console.print("결과 : ")
+
+    } catch(error){
+    }
+  }
 }
 
 export default App;
