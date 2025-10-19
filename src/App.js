1f5a0c6a2e899efbace97e7cba24792d04cc723d
JuHyeong <blane7777@naver.com>
2025-10-19 22:37:41 +0900

    feat(parsing): add input parsing feat

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ class App {
         "덧셈할 문자열을 입력해 주세요.\n"
       );
 
+      let numArray = this.parsing(inputStr)
+
       MissionUtils.Console.print("결과 : ")
 
     } catch(error){
+    }
+  }
+  
+  parsing(inputStr){
+    if(inputStr==="") return 0;
+    // 공백이면 결과로 0을 리턴
+
+    // 공백이 아니면
+    let checkStr = /^[1-9][0-9]*[,:][1-9][0-9]*([,:][1-9][0-9]*)*$/;
+    // (1 이상의 양수 + 구분자 + 1 이상의 양수)는 항상 포함하며
+    // 그 뒤에 (구분자 + 1이상의 양수)가 오는 형태를 검사
+
+    // 조건을 통과한 경우
+    if(checkStr.test(inputStr)){
+      return inputStr.split(/[,:]/);
     }
   }
 }
