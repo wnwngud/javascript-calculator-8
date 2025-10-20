981be8effdf2b9d37b9efcfa11d5cfc7030e5dbb
JuHyeong <blane7777@naver.com>
2025-10-20 12:16:57 +0900

    feat(parsing): add processing exception

--- src/App.js
+++ src/App.js
@@ -2,47 +2,63 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    try{
-      let inputStr = await MissionUtils.Console.readLineAsync(
-        "덧셈할 문자열을 입력해 주세요.\n"
-      );
+    let inputStr = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
 
-      let numArray = this.parsing(inputStr)
-      let result = this.calculate(numArray)
+    let numArray = this.parsing(inputStr);
+    let result = this.calculate(numArray);
 
-      MissionUtils.Console.print(`결과 : ${result}`)
-
-    } catch(error){
-    }
+    MissionUtils.Console.print(`결과 : ${result}`);
   }
   
-  parsing(inputStr){
+  parsing(inputStr) {
     // 공백이면 결과로 0을 리턴
     if(inputStr==="") return 0;
 
     // 공백이 아니면 파싱 작업 수행
-    let tempDelimiter = ",:";
+    let delimiter = ",:";
 
     // 커스텀 구분자 처리
-    if(inputStr.slice(0, 2) === '//' && inputStr.slice(3, 5) === '\\n'){
+    if(inputStr.startsWith('//')) {
+
+      // 커스텀 구분자 지정 형식 예외처리
+      // 커스텀 구분자의 길이가 2 이상인 경우 or
+      // '\n'을 제대로 입력하지 않은 경우
+      if(inputStr.slice(3,5) !== '\\n')
+        throw new Error("[ERROR] 옳바른 입력 형식이 아닙니다. ex\) //문자\\n");
+      
+      // 커스텀 구분자로 숫자를 사용한 경우
+      if(!isNaN(Number(inputStr[2]))) 
+        throw new Error("[ERROR] 커스텀 구분자는 문자만 입력해 주세요.");
+
+      let customDelimiter = inputStr[2]
+
       // 기존 구분자에 커스텀 구분자 추가
-      tempDelimiter += `${inputStr[2]}`;
+      delimiter += `${customDelimiter}`;
+
       inputStr = inputStr.slice(5);
     }
 
-    let delimiter = new RegExp(`[${tempDelimiter}]`);
-    let numArray = inputStr.split(delimiter);
+    // 이스케이프 처리가 별도로 필요한 문자들 처리
+    let checkEscape = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    delimiter = checkEscape(delimiter);
 
-    return(numArray)
+    let numArray = inputStr.split(new RegExp(`[${delimiter}]`));
+
+    return numArray;
   }
 
-  calculate(numArray){
+  calculate(numArray) {
     if (numArray === 0) return 0;
 
     let sum = 0;
 
-    // 문자를 정수로 변환 후 더하기 
     for(let i of numArray){
+      // 양수 및 문자열 연속 사용으로 인한 공백 검사
+      if(!/^[1-9][0-9]*$/.test(i))
+        throw new Error("[ERROR] 옳바른 계산식이 아닙니다.");
+
       sum+=Number(i);
     }
 
